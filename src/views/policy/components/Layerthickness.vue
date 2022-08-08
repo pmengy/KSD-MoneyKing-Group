fff36549ba0c44d645d0c9cc222fb8d36a6e4728
heimawl <1785969728@qq.com>
2022-08-08 20:17:50 +0800

    完成详情列表分页

--- src/views/policy/components/Layerthickness.vue
+++ src/views/policy/components/Layerthickness.vue
@@ -35,21 +35,49 @@
           </el-table-column>
         </el-table>
       </div>
+      <!-- 分页 -->
+        <div class="Pagination">
+          <p>共{{ totalCount }}条记录 第{{ pageIndex }}/{{ totalPage }}页</p>
+          <p>
+            <DkdButton
+              background="#d5ddf8"
+              :disabled="pageIndex === '1'"
+              color="#606266"
+              style="margin-right: 20px"
+              @click="lastPage"
+              >上一页</DkdButton
+            >
+            <DkdButton
+              :disabled="pageIndex === totalPage"
+              color="#606266"
+              background="#d5ddf8"
+              @click="nextPage"
+              >下一页</DkdButton
+            >
+          </p>
+        </div>
+      </el-card>
+    </div>
     </div>
   </el-dialog>
 </template>
 
 <script>
-import { getThemachineApi } from "@/api/index";
+import { getThemachineApi ,getThemachineApiF} from "@/api/index";
+import DkdButton from "@/components/DkdButton";
 export default {
   data() {
     return {
+      pageIndex: "",
+      totalPage: "",
+      totalCount: "",
       Layername: "九折",
       tableLabel: [
         { label: "点位名称", width: "300", prop: "nodeName" },
         { label: "售货机编号", width: "300", prop: "innerCode" },
       ],
       currentList: [],
+      LayId:''
     };
   },
   props: {
@@ -61,15 +89,44 @@ export default {
   },
 
   created() {},
-
+ components: { DkdButton },
   methods: {
+    // 关闭
     handleClose() {
       this.$emit("onClose");
     },
+    // 获取列表
     async getThemachineApi(val) {
       this.Layername = val.policyName;
+      this.LayId=val.policyId
       const res = await getThemachineApi(val.policyId);
       console.log(res);
+      this.pageIndex = res.data.pageIndex;
+      this.totalPage = res.data.totalPage;
+      this.totalCount = res.data.totalCount;
+      this.currentList = res.data.currentPageRecords;
+    },
+        // 下一页
+    async nextPage() {
+      const res = await getThemachineApiF(this.LayId,{
+        pageIndex: parseInt(this.pageIndex) + 1,
+      });
+      this.currentList = res.data.currentPageRecords;
+      this.pageIndex = parseInt(this.pageIndex) + 1;
+    },
+    // 获取上一页数据
+    async lastPage() {
+      if (parseInt(this.pageIndex) === 1) {
+        return this.$message({
+          showClose: true,
+          message: "没有上一页啦！！",
+          type: "warning",
+        });
+      }
+      const res = await getThemachineApiF(this.LayId,{
+        pageIndex: parseInt(this.pageIndex) - 1,
+      });
+      this.pageIndex = parseInt(this.pageIndex) - 1;
       this.currentList = res.data.currentPageRecords;
     },
   },
